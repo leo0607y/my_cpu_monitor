3d5fb4e2d56f53c6d0c9ca7d4011489481ad3a3b
leo0607y <reochimaru@gmail.com>
2025-12-29 19:21:32 +0900

    listener変更

--- my_cpu_monitor/listener_node.py
+++ my_cpu_monitor/listener_node.py
@@ -1,7 +1,6 @@
 ##!/usr/bin/env python
 # SPDX-FileCopyrightText: 2025 Reo Yamaguchi
 # SPDX-License-Identifier: BSD-3-Clause:
-
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32
@@ -9,20 +8,33 @@ from std_msgs.msg import Float32
 class CpuListenerNode(Node):
     def __init__(self):
         super().__init__('cpu_listener_node')
-        # 'cpu_usage'取得
         self.subscription = self.create_subscription(
-            Float32,
-            'cpu_usage',
-            self.listener_callback,
-            10)
-        self.get_logger().info('CPU Listener Node has started.')
+            Float32, 'cpu_usage', self.listener_callback, 10)
+        self.get_logger().info('--- SYSTEM RESOURCE MONITOR SUBSCRIPTION STARTED ---')
 
     def listener_callback(self, msg):
-        usage = msg.data
-        if usage > 80.0:
-            self.get_logger().warn(f'High CPU Usage Detected: {usage}%!')
+        u = msg.data
+        
+        # 2%刻み
+        total_steps = 50
+        filled = int(total_steps * u // 100)
+        bar = "#" * filled + "." * (total_steps - filled)
+
+        # ステータス判定
+        if u < 30.0:
+            status = "LOW   "
+            log = self.get_logger().info
+        elif u < 70.0:
+            status = "MID   "
+            log = self.get_logger().info
+        elif u < 90.0:
+            status = "HIGH  "
+            log = self.get_logger().warn
         else:
-            self.get_logger().info(f'Received CPU Usage: {usage}%')
+            status = "ALERT "
+            log = self.get_logger().error
+
+        log(f"[{status}] [{bar}] {u:5.1f}%")
 
 def main(args=None):
     rclpy.init(args=args)
@@ -32,10 +44,6 @@ def main(args=None):
     except KeyboardInterrupt:
         pass
     finally:
-        # エラー回避
         if rclpy.ok():
             node.destroy_node()
             rclpy.shutdown()
-
-if __name__ == '__main__':
-    main()
