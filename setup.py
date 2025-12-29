1521d0a899350c4a238a5a99c805a43a328b612c
leo0607y <reochimaru@gmail.com>
2025-12-29 15:15:46 +0900

    setup修正

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Launchファイルをインストール対象に追加
         (os.path.join('share', package_name), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
